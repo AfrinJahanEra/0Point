866416f374cc352b87bbf4277745f8f4a2a39818
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2026-01-18 20:42:11 +0600

    Delete Server/crossPlatform/management/commands/sync_cf.py

--- Server/crossPlatform/management/commands/sync_cf.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from crossPlatform.services.codeforces import sync_codeforces_contests
-
-class Command(BaseCommand):
-    help = "Sync Codeforces contests"
-
-    def handle(self, *args, **kwargs):
-        sync_codeforces_contests()
-        self.stdout.write(self.style.SUCCESS("Codeforces synced"))
