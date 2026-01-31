80b82915df6be4d5018301931cc5c5dcc95d24ba
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2026-01-31 23:45:53 +0600

    Added account url

--- Server/zeropoint/urls.py
+++ Server/zeropoint/urls.py
@@ -7,6 +7,7 @@ from django.views.static import serve
 
 urlpatterns = [
     path("auth/", include("account.urls")),
+    path("account/", include("account.urls")),
     path("", include("contest.urls")),
     path("", include("submission.urls")),
     path("", include("problem.urls")), 
