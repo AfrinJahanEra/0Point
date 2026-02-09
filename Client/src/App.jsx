5e2c166b7114743369354b6c61b00dc2bf9573ad
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2026-02-09 19:19:38 +0600

    added a new route for contest history page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -38,6 +38,7 @@ import TestContestLeaderboard  from './pages/TestContestLeaderboard';
 import TestContestSubmission from './pages/TestContestSubmission';
 import BlogDetail from './pages/BlogDetail';
 import Recording from './pages/Recording';
+import ContestHistory from './pages/ContestHistory';
 // Layout component that includes Header and Footer only
 const Layout = ({ children }) => (
   <>
@@ -299,6 +300,12 @@ function App() {
             <Route path="/submissions" element={
               <NavLayout>
                 <Submissions />
+              </NavLayout>
+            } />
+
+            <Route path="/contest-history" element={
+              <NavLayout>
+                <ContestHistory />
               </NavLayout>
             } />
            
