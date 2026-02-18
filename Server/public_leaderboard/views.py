329fce8b0363e8354b2cd6d13e7dfc5b73f9d653
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2026-02-18 21:29:53 +0600

    Update views.py

--- Server/public_leaderboard/views.py
+++ Server/public_leaderboard/views.py
@@ -7,4 +7,35 @@ from contest.models import ContestRegistration
 from .serializers import LeaderboardSerializer
 
 class GlobalLeaderboardView(APIView):
-  pass
+
+    def get(self, request):
+
+        # Fetch active users sorted by rating descending
+        users = Account.objects(
+            is_deleted=False,
+            is_inactive=False
+        ).order_by('-rating')
+
+        leaderboard_data = []
+        current_rank = 1
+
+        for index, user in enumerate(users):
+
+            # Count contests participated
+            contests_count = ContestRegistration.objects(
+                user=user
+            ).count()
+
+            leaderboard_data.append({
+                "username": user.name,
+                "total_points": user.rating,
+                "department": user.department,
+                "contests_participated": contests_count,
+                "rank": current_rank
+            })
+
+            current_rank += 1
+
+        serializer = LeaderboardSerializer(leaderboard_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
