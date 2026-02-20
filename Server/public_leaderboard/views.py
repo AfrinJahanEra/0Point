20e6f839011aeda6d9e40fddf8b2c1c0739255c4
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2026-02-20 19:10:06 +0600

    Add leaderboard server files

--- Server/public_leaderboard/views.py
+++ Server/public_leaderboard/views.py
@@ -1,3 +1,5 @@
+# leaderboard/views.py
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,6 +7,7 @@ from rest_framework import status
 from account.models import Account
 from contest.models import ContestRegistration
 from .serializers import LeaderboardSerializer
+
 
 class GlobalLeaderboardView(APIView):
 
@@ -38,4 +41,3 @@ class GlobalLeaderboardView(APIView):
 
         serializer = LeaderboardSerializer(leaderboard_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
