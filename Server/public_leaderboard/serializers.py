e48731b0e6f777e3d73d50cd7479999b5501612a
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2026-02-23 21:55:34 +0600

    Add user_id field to LeaderboardSerializer

--- Server/public_leaderboard/serializers.py
+++ Server/public_leaderboard/serializers.py
@@ -3,8 +3,10 @@
 from rest_framework import serializers
 
 class LeaderboardSerializer(serializers.Serializer):
+    user_id = serializers.CharField()
     username = serializers.CharField()
     total_points = serializers.IntegerField()
     department = serializers.CharField()
     contests_participated = serializers.IntegerField()
     rank = serializers.IntegerField()
+
