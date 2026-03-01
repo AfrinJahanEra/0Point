7e2d695ef09a96b8bb6a5103639296a1c2d311ec
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2026-03-01 09:11:41 +0600

    Update leaderboard urls.py

--- Server/public_leaderboard/urls.py
+++ Server/public_leaderboard/urls.py
@@ -1,8 +1,10 @@
 # leaderboard/urls.py
 
 from django.urls import path
-from .views import GlobalLeaderboardView
+from .views import GlobalLeaderboardView, LeaderboardMinimalView
 
 urlpatterns = [
     path('leaderboard/', GlobalLeaderboardView.as_view(), name='leaderboard'),
+    # In your urls.py
+path('leaderboard/minimal/', LeaderboardMinimalView.as_view(), name='leaderboard-minimal'),
 ]
